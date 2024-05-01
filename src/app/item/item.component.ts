c6a796330a324ccdc7f12cea26c719f7782220fa
Isis Castro Perez Bellan <isisbelllan@gmail.com>
2024-05-01 12:36:24 -0300

    first commit

---
+++ src/app/item/item.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Tarefa } from "../tarefa";
+@Component({
+ selector: 'app-item',
+ templateUrl: './item.component.html',
+ styleUrls: ['./item.component.css']
+})
+export class ItemComponent {
+ emEdicao = false;
+ @Input() tarefa: Tarefa = new Tarefa("", false);
+ @Output() removeItem = new EventEmitter<string>();
+ @Output() modificaTarefa = new EventEmitter();
+
+ enviarEvento()
+    {
+        this.removeItem.emit(this.tarefa.descricao);
+    }
+ 
+
+}
